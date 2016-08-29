d1580188a8914c288771970e8e6e14b73565f998
wcong <wc19920415@gmail.com>
2016-08-29 21:05:35 +0800

    add start limit

--- src/main/java/org/wcong/ants/http/handler/HttpServerDocumentAnalysisHandler.java
+++ src/main/java/org/wcong/ants/http/handler/HttpServerDocumentAnalysisHandler.java
@@ -12,6 +12,7 @@ import org.wcong.ants.document.DocumentTerms;
 import org.wcong.ants.http.HttpServerHandler;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -35,6 +36,8 @@ public class HttpServerDocumentAnalysisHandler extends HttpServerHandler impleme
             String spider = null;
             String index = null;
             String field = null;
+            int start = 0;
+            int limit = 500;
             Map<String, List<String>> parameterMap = query.parameters();
             List<String> spiderList = parameterMap.get("spider");
             if (spiderList != null && !spiderList.isEmpty()) {
@@ -48,8 +51,25 @@ public class HttpServerDocumentAnalysisHandler extends HttpServerHandler impleme
             if (fieldList != null && !fieldList.isEmpty()) {
                 field = fieldList.get(0);
             }
+            List<String> startList = parameterMap.get("start");
+            if (startList != null && !startList.isEmpty()) {
+                start = Integer.parseInt(startList.get(0));
+            }
+            List<String> limitList = parameterMap.get("limit");
+            if (limitList != null && !limitList.isEmpty()) {
+                limit = Integer.parseInt(limitList.get(0));
+            }
             if (spider != null && index != null) {
                 DocumentTerms documentTerms = documentReader.sumTerms(spider, index, field);
+                if (start >= documentTerms.getTerms().size()) {
+                    documentTerms.setTerms(Collections.<DocumentTerms.DocumentTerm>emptyList());
+                } else {
+                    int end = start + limit;
+                    if (end > documentTerms.getTerms().size()) {
+                        end = documentTerms.getTerms().size();
+                    }
+                    documentTerms.setTerms(documentTerms.getTerms().subList(start, end));
+                }
                 data = objectMapper.writeValueAsBytes(documentTerms);
             }
         } catch (IOException e) {
